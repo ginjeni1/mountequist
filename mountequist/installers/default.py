26b1bf9944b8da2d7695185f46bde6e5a3ac153d
Christian Lecours <christian.lecours@optelgroup.com>
2018-02-25 17:02:26 -0500

    Proper unsupported OS errors.

--- mountequist/installers/default.py
+++ mountequist/installers/default.py
@@ -1,3 +1,5 @@
+import sys
+
 from mountequist.installers.base import Installer
 from mountequist.installers.web import WindowsWeb
 
@@ -7,5 +9,10 @@ def get_default_installer():
     :return: An instance of the default installer.
     :rtype: Installer
     """
-    # TODO Currently only supports Windows
-    return WindowsWeb()
+
+    if sys.platform == "win32":
+        return WindowsWeb()
+    elif "linux" in sys.platform:
+        raise NotImplementedError("Linux not yet supported.")
+    else:
+        raise NotImplementedError("OS not supported.")
